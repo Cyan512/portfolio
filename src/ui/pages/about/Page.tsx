01233f4c580321880ee6ce8e1f9002a43f760c15
Erick6751 <erick.founders@gmail.com>
2026-02-09 19:18:31 -0500

    add md.

--- src/ui/pages/about/Page.tsx
+++ src/ui/pages/about/Page.tsx
@@ -1,9 +1,11 @@
 import {MainGridLayout} from "@/ui/templates/main-grid-layout.tsx";
+import ReactMarkdown from "react-markdown";
+import content from "@/content/readme.md?raw";
 
 export default function AboutPage() {
     return (
         <MainGridLayout>
-            About
+            <ReactMarkdown>{content}</ReactMarkdown>;
         </MainGridLayout>
     )
 }
